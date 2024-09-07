0899f51438d88ea181a738acdbf4338e68a2d07f
obaydullaa <cr7obaydul@gmail.com>
2024-09-07 15:15:50 +0600

    functin asss object.

--- Day_10__More_Objects_part2_(22-02-2022)/07._More_Objects/07.12._Functions_as_a_object.js
+++ Day_10__More_Objects_part2_(22-02-2022)/07._More_Objects/07.12._Functions_as_a_object.js
@@ -7,6 +7,8 @@
  * 1. Premitive 2. Reference => i. Objecr ii. Funcrion iii. array
  */
 
+// example: 01 
+
 function sayHi() {
     console.log('Hi');
 }
@@ -14,5 +16,27 @@ function sayHi() {
 sayHi.firstName = 'obaydul';
 sayHi.lasttName = 'obaydul';
 
-
 sayHi();
+
+
+// example: 02
+
+function greet() {
+    console.log('Hi');
+}
+
+greet.name = 'function';
+console.log(greet.name)
+
+
+greet();
+
+console.log(typeof greet)
+console.log(greet instanceof Object)
+
+
+let body = "return 'Hi' ";
+
+let funcAsObj = new Function('greet', body )
+
+console.log(funcAsObj())
