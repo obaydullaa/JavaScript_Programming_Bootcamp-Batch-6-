be6edb185bddac701f630a6c698c2cebbdb0d3d8
obaydullaa <cr7obaydul@gmail.com>
2024-09-07 12:35:56 +0600

    factory function practices.

--- Day_10__More_Objects_part2_(22-02-2022)/07._More_Objects/07.3._Factory_Function.js
+++ Day_10__More_Objects_part2_(22-02-2022)/07._More_Objects/07.3._Factory_Function.js
@@ -2,6 +2,43 @@
  * 7.3.Factory Function
  */
 
+
+
+const profile = {
+    fistName: 'obaydul',
+    lastName: 'Islam',
+    email: 'obaydul@gmail.com',
+    // fullName: function() {  // Old js
+    //     return profile.fistName + ' ' + profile.lastName;
+    // },
+    fullName() { // Modal js
+        console.log(this)
+        return this.fistName + ' ' + this.lastName;
+    }
+}
+
+console.log(profile.fistName);
+
+// it's call factory function 
+function printProfile(firstName, lastName, email) {
+    return {
+        fistName,
+        lastName,
+        email,
+        fullName() {
+            console.log(this)
+            return this.fistName + ' ' + this.lastName;
+        }
+    }
+}
+
+
+console.log(printProfile('obaydul', 'islam', 'obaydul@gmail.com'))
+console.log(printProfile('korim', 'korim', 'korim@gmail.com'))
+
+
+
+
 function product(name, price, availability) {
     return {
         name,
