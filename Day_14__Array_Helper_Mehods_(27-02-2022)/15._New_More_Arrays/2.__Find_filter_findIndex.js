a0859c6d0e9723e7ec17c973ee44870a34aa8669
obaydullaa <cr7obaydul@gmail.com>
2024-09-09 00:21:16 +0600

    array hlper method find filter and findindex

---
+++ Day_14__Array_Helper_Mehods_(27-02-2022)/15._New_More_Arrays/2.__Find_filter_findIndex.js
@@ -0,0 +1,81 @@
+/**
+ * 1.Array helper method map
+ * ---------------------------
+ */
+
+// Map 
+// It works with an array 
+// return an array with all (?) substitutes 
+// results depends ho you manipulate the data
+// must return from the call back function 
+// doesn't mutate the original array
+
+// let arr = [1, 2, 3, 4];
+
+// let result = arr.map(function (elm){
+//     console.log(elm)
+//     return elm * 2;
+// });
+
+// console.log(result);
+// console.log(arr);
+
+/**
+ * 2.Find filter findIndex
+ * ---------------------------
+ */
+
+// It works with an array 
+// return an array 
+// results depends based on condition written inside callback function 
+// must return from the call back function 
+// doesn't mutate the original array
+
+
+// let arr = [1, 2, 3, 4];
+
+// let result = arr.filter( (elm) =>  elm % 2 === 0);
+
+// console.log(result);
+// console.log(arr);
+
+
+/**
+ * 2.Find 
+ * ---------------------------
+ */
+
+// It works with an array 
+// return a value based on condition
+// results depends based on condition written inside callback function 
+// must return from the call back function 
+// doesn't mutate the original array
+
+
+// let arr = [1, 2, 3, 4];
+
+// let result = arr.find( (elm) =>  elm % 2 === 0);
+
+// console.log(result);
+// console.log(arr);
+
+
+
+/**
+ * 2.findIndex
+ * ---------------------------
+ */
+
+// It works with an array 
+// return an index based on condition
+// results depends based on condition written inside callback function 
+// must return from the call back function 
+// doesn't mutate the original array
+
+
+// let arr = [1, 2, 3, 4];
+
+// let result = arr.findIndex( (elm) =>  elm % 2 === 0);
+
+// console.log(result);
+// console.log(arr);
