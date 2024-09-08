f4fadd7118d9e623a919d4510ba7d99f3ba1a546
obaydullaa <cr7obaydul@gmail.com>
2024-09-08 13:28:54 +0600

    object destructiring

--- Day_13__14._Destructuring_Rest_Spread_(24-02-2022)/1.__Array_destrucuring.js
+++ Day_13__14._Destructuring_Rest_Spread_(24-02-2022)/1.__Array_destrucuring.js
@@ -15,6 +15,7 @@ let profile = [undefined ,30, true, 'programmer'];
 // let [firsName, age, isLearner, profession] = profile;
 // let [firsName, , isLearner, profession] = profile;
 
+// Order maintain
 let [firstName = 'Anis', age , isLearner, profession] = profile;
 
 console.log(firstName, age, isLearner, profession);
--- Day_13__14._Destructuring_Rest_Spread_(24-02-2022)/2.__Object_destrucuring.js
+++ Day_13__14._Destructuring_Rest_Spread_(24-02-2022)/2.__Object_destrucuring.js
@@ -1,5 +1,24 @@
 /**
- *  1.Array destructuring
+ *  2.Object destrucuring
+ * 
  */
 
-let profiler = ['obaydul',30, true. 'programmer'];
+const profile = {
+    fistName: 'obaydul',
+    lastName: 'Islam',
+    email: 'obaydul@gmail.com',
+    personTitle: 'Mr',
+    fullName() {  
+        return this.fistName + ' ' + this.lastName;
+    }
+}
+
+// console.log(profile.fistName)
+// console.log(profile.lastName)
+
+// No order maintain
+let {fistName, edu ='University', lastName, email, personTitle, fullName}   = profile;
+
+console.log(fistName, lastName)
+console.log(fullName.call(profile), edu)
+
