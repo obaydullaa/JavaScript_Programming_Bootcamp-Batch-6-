ba1d312105d1a7e68f4a6878daaa3c1e64625c36
obaydullaa <cr7obaydul@gmail.com>
2024-09-08 13:14:54 +0600

    array destructuring .

---
+++ Day_12__14._Destructuring_Rest_Spread_(24-02-2022)/1.__Array_destrucuring.js
@@ -0,0 +1,20 @@
+/**
+ *  1.Array destructuring
+ */
+
+let profile = [undefined ,30, true, 'programmer'];
+
+// let firsName = profile[0];
+// let age = profile[1];
+// let isLearner = profile[2];
+// let profession = profile[3];
+
+// array destructuring 
+//--------------------- 
+// avoid any key 
+// let [firsName, age, isLearner, profession] = profile;
+// let [firsName, , isLearner, profession] = profile;
+
+let [firstName = 'Anis', age , isLearner, profession] = profile;
+
+console.log(firstName, age, isLearner, profession);
