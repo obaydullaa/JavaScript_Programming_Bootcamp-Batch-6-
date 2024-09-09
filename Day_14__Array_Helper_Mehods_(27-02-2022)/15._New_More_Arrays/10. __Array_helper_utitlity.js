9eee7722ebe13a7cdac311a8225eea0c55759669
obaydullaa <cr7obaydul@gmail.com>
2024-09-09 16:54:49 +0600

    array form and array.of

---
+++ Day_14__Array_Helper_Mehods_(27-02-2022)/15._New_More_Arrays/10. __Array_helper_utitlity.js
@@ -0,0 +1,27 @@
+
+/**
+ * 10. Array helper utility =>  Array.Form & Array.of 
+ *----------------------------
+
+ */
+
+ // Array.Form 
+//  let arr = [1, 2, 3, 4]
+
+// function printNumb() {
+//   console.log( typeof arguments)
+//   console.log(Array.isArray(arguments))
+//   // Convert Real to Array 
+//   let arr = Array.from(arguments)
+
+//   console.log( Array.isArray(arr))
+// }
+// printNumb(1, 2, 3, 4);
+
+
+
+ // Array.of 
+//  let array = Array.of(2, 3, 4, 5, 6)
+ let array = new Array(2)
+
+ console.log(array)
