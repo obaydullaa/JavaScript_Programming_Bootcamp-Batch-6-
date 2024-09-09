3e09b5437e7a33ff4f2e0229f8d460223e10c15b
obaydullaa <cr7obaydul@gmail.com>
2024-09-09 17:32:20 +0600

    js dom

--- Day_14__Array_Helper_Mehods_(27-02-2022)/15._New_More_Arrays/10. __Array_helper_utitlity.js
+++ Day_14__Array_Helper_Mehods_(27-02-2022)/15._New_More_Arrays/10. __Array_helper_utitlity.js
@@ -6,6 +6,9 @@
  */
 
  // Array.Form 
+ //Array.from() একটি array-like (যেমন string, NodeList, বা Set) বা iterable object (যেমন Map বা arguments) কে প্রকৃত array তে রূপান্তর করে। 
+//  এটি বিশেষ করে উপকারী যখন তোমার কাছে একটি array-like object থাকে এবং সেটিকে array হিসাবে ব্যবহার করতে চাও।
+
 //  let arr = [1, 2, 3, 4]
 
 // function printNumb() {
@@ -17,11 +20,20 @@
 //   console.log( Array.isArray(arr))
 // }
 // printNumb(1, 2, 3, 4);
+// let stringToArray = Array.from('Hello');
+// console.log(stringToArray);  // Output: ['H', 'e', 'l', 'l', 'o']
 
 
 
  // Array.of 
 //  let array = Array.of(2, 3, 4, 5, 6)
- let array = new Array(2)
+// Array.of() হলো একটি মেথড যা যেকোনো সংখ্যা বা এলিমেন্ট নিয়ে একটি array তৈরি করে। এটি array literals এর মতো কাজ করে, তবে একটি গুরুত্বপূর্ণ পার্থক্য হলো, যদি শুধু একটি সংখ্যা পাস করা হয়, তা হলে এটিকে সংখ্যা হিসেবে নেয় এবং তা একটি array এর মধ্যে রাখে, যেখানে new Array() এটা একটি নির্দিষ্ট সাইজের array তৈরি করে।
+//  let array = new Array(2)
 
- console.log(array)
+//  console.log(array)
+
+//  let singleElementArray = Array.of(7);
+// console.log(singleElementArray);  // Output: [7]
+
+// let multipleElementsArray = Array.of(1, 2, 3, 4);
+// console.log(multipleElementsArray);  // Output: [1, 2, 3, 4]
---
+++ Day_15__Dom_js_(03-03-2022)/Dom/10.1_Introduction_to_DOM.js
@@ -0,0 +1,8 @@
+/**
+ * 10.1_Introduction to DOM
+ * 
+ * DOM =>  Dcocument Object Model
+ * DOM Use some kind of Interaction
+ * 
+ * The browser turn every HTML tag into a javaScript object that we can manipulate.
+ */
