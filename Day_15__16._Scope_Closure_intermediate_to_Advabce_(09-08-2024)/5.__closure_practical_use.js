c521c38cc8498d1c0f214e696e83f15559941131
obaydullaa <cr7obaydul@gmail.com>
2024-09-09 21:42:44 +0600

    scope closer use case

--- Day_15__16._Scope_Closure_intermediate_to_Advabce_(09-08-2024)/5.__closure_practical_use.js
+++ Day_15__16._Scope_Closure_intermediate_to_Advabce_(09-08-2024)/5.__closure_practical_use.js
@@ -1,21 +1,35 @@
 /**
- * 4.closer scope
+ * 5.closure practical use
  * ------------------------------
  *  Bubble Up (nice thake uporer thak search korte korte jai )
  */
 // inner ফাংশনটি return হয় এবং partialPrintName এ সংরক্ষিত হয়। তবে এটা গুরুত্বপূর্ণ যে, inner ফাংশনের সাথে সাথে firstName ভ্যারিয়েবলও scope এর কারণে মেমোরিতে থেকে যায়, অর্থাৎ inner ফাংশন firstName এর মান জানে এবং এটা যেকোনো সময় ব্যবহার করতে পারে।
 // inner ফাংশনের এই আচরণকে বলা হয় closure। Closure মানে হচ্ছে, একটি ফাংশন তার চারপাশের scope-এর ভ্যারিয়েবলগুলোর অ্যাক্সেস রাখে, এমনকি যখন সেই ফাংশনটি তার মূল স্কোপের বাইরে কল করা হয়।
 
-function printName() {
-    let firstName = 'Obaydulla';
-   return function inner(lastName) {
-       return firstName + ' ' + lastName;
+
+// function printName() {
+//     let firstName = 'Obaydulla';
+//    return function inner(lastName) {
+//        return firstName + ' ' + lastName;
+//     }
+// }
+
+
+// let partialPrintName = printName()
+
+
+// console.log(partialPrintName('Hasan'))
+
+// to protect variable, data 
+function gamePlay(num) {
+    let lifeSpan = num;
+    return () => {
+        console.log('.....Playing....');
+        lifeSpan--;
+        console.log('....Remaining Playing life....', lifeSpan);
     }
+    return play;  
 }
 
-
-let partialPrintName = printName()
-
-
-console.log(partialPrintName('Hasan'))
-
+let play = gamePlay(4);  
+console.log(play());  
