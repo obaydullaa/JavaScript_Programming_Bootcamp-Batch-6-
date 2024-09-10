042e405948f5ea525def6a10362bae9ad0cd78fb
obaydullaa <cr7obaydul@gmail.com>
2024-09-10 13:24:03 +0600

    Synchronous vs asynchronus in js.

---
+++ Day_23__Tut-Tut_Project__(14-03-2022)/Tut-Tut_Project/assets/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Required Thid Project
+ ==================================
+  
+    1. Input must not have 0/250 words
+    2. 0/250 (More professional and little challenging)
+    
+    3. 2nd feature (1:30pm)
+    4. Generate time during adding tweet
+ */
