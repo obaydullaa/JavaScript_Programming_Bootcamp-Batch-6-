d79189474780b2a37b56ef6a903165f9e47c1c7e
obaydullaa <cr7obaydul@gmail.com>
2024-09-10 11:09:44 +0600

    add js luck game

--- Day_16__Dom_js_(03-03-2022)/Dom/Dom_Practice/assets/js/dom.js
+++ Day_16__Dom_js_(03-03-2022)/Dom/Dom_Practice/assets/js/dom.js
@@ -157,12 +157,199 @@
  *10 - Doctype
  */
 
-let val;
-val = document.querySelector('.product-collection');
+// let val;
+// val = document.querySelector('.product-collection');
 // val = val.childNodes[1].nodeName;
 
-if (val.childNodes[1].nodeType !== 3) {
-    val.childNodes[1].classList.add('MyClass');
+// if (val.childNodes[1].nodeType !== 3) {
+//     val.childNodes[1].classList.add('MyClass');
+// }
+
+// console.log(val);
+
+/**
+ * 10.9_Creating element
+ */
+// <li class="product-collection-item">Sneakers</li>
+
+// let val;
+// val = document.querySelector('.product-collection');
+
+// const ul = document.querySelector('ul');
+// const li = document.createElement('li');
+
+// li.className = 'product-collection-item';
+// li.appendChild(document.createTextNode('myItem'))
+//     // ul.appendChild(li); // last item add kore.
+// ul.prepend(li); // first  add kore.
+
+// console.log(ul);
+
+
+/**
+ * 10.10_Replacing and removing element
+ =============================================
+ */
+
+// const oldHeading = document.querySelector(".h1");
+// const newHeading = document.createElement("h1");
+
+// newHeading.appendChild(document.createTextNode("My Updated product list"));
+
+// const container = document.querySelector(".container");
+
+// container.replaceChild(newHeading, oldHeading);
+// oldHeading.replaceWith(newHeading);
+
+// // ** Remove Last Element
+
+// const list = document.querySelector('.product-collection');
+// // list.lastElementChild.remove(); // remove element
+
+// list.removeChild(list.lastElementChild);
+
+// console.log(list); // For last child select
+
+
+
+/**
+ * 10.11_Event introduction
+ ======================================
+ * Events are everywhere.
+ *
+ * So Many Events !
+ * MDN list over 300 different events!
+ * Example => click, Mouseover, dblclick, keypress, drag
+ * 
+ * 
+ */
+
+
+/**
+ * 10.12_Event in practice
+ =====================================
+ */
+
+// const h1 = document.querySelector('h1');
+// h1.addEventListener('click', function(evt) {
+
+//     console.log('Type:', evt.type);
+//     console.log('Target', evt.target);
+// console.log('offset-X:', evt.offsetX);
+// console.log('offset-Y:', evt.offsetY);
+//     console.log('client-x:', evt.clientX);
+//     console.log('client-y:', evt.clientY);
+//     console.log('Clicked H1');
+// });
+
+
+/**
+ * This in formation use other elements like li then  follow below:-
+ */
+
+// const h1 = document.querySelector('h1');
+// const li = document.querySelector('li');
+// const lis = document.querySelectorAll('li');
+
+// const form = document.forms[0];
+// const input = document.getElementById('productName');
+
+// function evtInformation(evt) {
+//     evt.preventDefault();
+//     console.log('Type:', evt.type);
+//     console.log('Target:', evt.target);
+//     console.log('Input Value:', input.value);
+
+// console.log('Target Value', evt.target.innerText);
+
+// console.log('offset-X:', evt.offsetX);
+// console.log('offset-Y:', evt.offsetY);
+// console.log('client-x:', evt.clientX);
+// console.log('client-y:', evt.clientY);
+// console.log('Clicked H1');
+// }
+
+// h1.addEventListener("click", evtInformation);
+
+// lis.forEach(li => {
+
+//     li.addEventListener("click", evtInformation)
+// });
+
+// form.addEventListener('submit', evtInformation);
+
+
+/**
+ * 10.13_It's your time to practice
+ ============================================
+ * Event Bubbling   => Up or Children to Parent.
+ * Event Delegation => Parent to children.
+ */
+
+//  * Event Bubbling   => Up or Children to Parent.
+// const li = document.querySelector('li');
+// const ul = document.querySelector('ul');
+// const container = document.querySelector('.container');
+
+// li.addEventListener('click', () => {
+//     console.log('You Clicked li');
+// });
+// ul.addEventListener('click', () => {
+//     console.log('You Clicked ul');
+// });
+// container.addEventListener('click', () => {
+//     console.log('You Clicked container');
+// });
+// console.log(li);
+
+
+//  * Event Delegation => Parent to children.
+
+// const li = document.querySelector('li');
+// const ul = document.querySelector('ul');
+// const container = document.querySelector('.container');
+
+
+// container.addEventListener('click', (evt) => {
+//     if (evt.target.id === 'sample') {
+//         evt.target.classList.add('custom');
+//     }
+// });
+// console.log(li);
+
+
+/**
+ * 10.15_Event delegation in practice
+ =========================================
+ * 
+ * 
+ */
+
+const ul = document.querySelector('ul');
+const form = document.forms[0];
+const input = document.getElementById('productName');
+
+function evtInformation(evt) {
+    evt.preventDefault();
+    console.log('Type:', evt.type);
+    console.log('Target:', evt.target);
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    li.className = 'product-collection-item';
+    ul.appendChild(li);
+    // console.log('Target Value:', evt.target.innerText);
 }
 
-console.log(val);
+form.addEventListener('submit', evtInformation);
+
+
+const lis = document.querySelectorAll('li');
+
+ul.addEventListener('click', (evt) => {
+    if (evt.target.className = 'product-collection-item') {
+        console.log(evt.target);
+    }
+});
+// lis.forEach(li => {
+//     li.addEventListener('click', evtInformation);
+// });
