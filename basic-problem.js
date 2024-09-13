6d67bbf2d67bfc8992247d47e14fcb05879567e5
obaydullaa <cr7obaydul@gmail.com>
2024-09-13 07:16:32 +0600

    two number and return theri sum.

---
+++ basic-problem.js
@@ -0,0 +1,20 @@
+/**
+ * Date: 13 september 2024
+ * 
+ * Here is a list of JavaScript basic problems 
+ */
+
+
+/**
+ * Problem 01: 
+ * 
+ *  Write a function that takes two numbers and returns their sum.
+ * 
+ */
+
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(5, 5));
